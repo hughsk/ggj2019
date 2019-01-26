7987b0c9549d6ce8840fbf6ffdeae4dc53007972
Hugh Kennedy <hughskennedy@gmail.com>
2019-01-27 10:08:30 +1100

    Materials

--- Assets/Scripts/TextBubble.cs
+++ Assets/Scripts/TextBubble.cs
@@ -32,7 +32,7 @@ public class TextBubble : MonoBehaviour {
         yield return new WaitForSeconds(0.04f);
       }
 
-      yield return new WaitForSeconds(2f);
+      yield return new WaitForSeconds(1.2f);
       writing = false;
       yield return new WaitForSeconds(0.5f);
       waiting = false;
