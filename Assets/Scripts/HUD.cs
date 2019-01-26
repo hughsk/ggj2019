7db96db2b491098572f72c1b7433b257cd0b752a
Rune <43809212+Runeatic@users.noreply.github.com>
2019-01-27 01:02:23 +1100

    HUD/PauseMenu/ScoreScreen in game scene

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -3,40 +3,165 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System.Threading;
+using UnityEngine.SceneManagement;
 
 public class HUD : MonoBehaviour
 {
     public Text scoreText;
-    private int score;
+    public Text finalScore;
+    public float score;
+    public float pointValue;
     public int timeLeft;
     public Text countdown;
+    private string scoreDesc;
+    bool keepTiming;
 
+    public string LevelName;
+
+    GameObject[] pauseObjects;
+    GameObject[] scoreObjects;
 
     void Start()
-    {        
+    {
         StartCoroutine("LoseTime");
         Time.timeScale = 1;
+
+        pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");        
+        scoreObjects = GameObject.FindGameObjectsWithTag("ShowOnFinish");
+
+        hideScore();
+        hidePaused();
     }
 
     void Update()
     {
         countdown.text = ("" + timeLeft);
+        score = score + pointValue;
         SetScoreText();
-    }
 
-    /*void OnTriggerEnter(Collider other) 
-    {
-        if (other.gameObject.CompareTag ( "Pick Up"))
+        if (timeLeft == 0)
         {
-            other.gameObject.SetActive (false);
-            score = score + 1;
-            SsetScoreText ();
+            showScore();
         }
-    }*/
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+                showPaused();
+            }
+            else if (Time.timeScale == 0)
+            {
+                Time.timeScale = 1;
+                hidePaused();
+            }
+        }
+    }
 
     void SetScoreText()
     {
-        scoreText.text = "$" + score.ToString("f0");
+       scoreText.text = "$" + score.ToString("f0");
+    }
+
+    public void showScore()
+    {
+        foreach (GameObject g in scoreObjects)
+        {
+            g.SetActive(true);
+        }
+
+        finalScore.text = finalScore.text = "You earned: $" + score.ToString() + ", just enough to " + scoreDesc;
+
+        if (score >= 0 && score <= 499)
+        {
+            scoreDesc = "live at home!!";
+        }
+
+        else if (score >= 500 && score <= 99999)
+        {
+            scoreDesc = "get a plane ticket!!";
+        }
+
+        else if (score >= 100000 && score <= 149999)
+        {
+            scoreDesc = "buy an apartment!!";
+        }
+
+        else if (score >= 150000 && score <= 299999)
+        {
+            scoreDesc = "live in a motor home!!";
+        }
+
+        else if (score >= 300000 && score <= 499999)
+        {
+            scoreDesc = "own a house!!";
+        }
+
+        else if (score >= 500000 && score <= 999999)
+        {
+            scoreDesc = "live free in a boat house!!";
+        }
+
+        else if (score >= 1000000 && score <= 1999999)
+        {
+            scoreDesc = "own a farm!!";
+        }
+
+        else if (score >= 2000000 && score <= 2999999)
+        {
+            scoreDesc = "have a private mansion!!";
+        }
+
+        else if (score >= 3000000 && score <= 3999999)
+        {
+            scoreDesc = "get an actual plane!!";
+        }
+
+        else if (score >= 4000000 && score <= 15999999)
+        {
+            scoreDesc = "casually purchase a skyscraper!!";
+        }
+
+        else if (score >= 16000000 && score <= 999999999)
+        {
+            scoreDesc = "purchase a private island!!";
+        }
+
+    }
+
+    public void hideScore()
+    {
+        foreach (GameObject g in scoreObjects)
+        {
+            g.SetActive(false);
+        }
+    }
+
+    public void showPaused()
+    {
+        foreach (GameObject g in pauseObjects)
+        {
+            g.SetActive(true);
+        }
+    }
+
+    public void hidePaused()
+    {
+        foreach (GameObject g in pauseObjects)
+        {
+            g.SetActive(false);
+        }
+    }
+
+    public void OnMenu_Clicked()
+    {
+        SceneManager.LoadScene(LevelName);
+    }
+
+    public void OnReturn_Clicked()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     IEnumerator LoseTime()
