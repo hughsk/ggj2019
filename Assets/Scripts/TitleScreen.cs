1a0c4623ee93bbb253eb478438a0267551cf374a
Rune <43809212+Runeatic@users.noreply.github.com>
2019-01-26 18:21:36 +1100

    title screen works

---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour
+{
+    public string LevelName;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(LevelName); //needs levelname to be loaded into build settings
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+    }
+}
